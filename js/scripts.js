45ecadd3723b82d95f95cf39c9ece4a33f764170
GuillaumeYves <guillaume-las@hotmail.fr>
2024-07-23 21:31:22 +0200

    first commit with progress

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(window).scroll(function () {
+  var scroll = $(window).scrollTop();
+  if (scroll >= 50) {
+    $("#nav").addClass("scrolled");
+  } else {
+    $("#nav").removeClass("scrolled");
+  }
+});
