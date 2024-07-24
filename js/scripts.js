345c2ed12b5364c28c7deed8ad3e0d8e49e34415
GuillaumeYves <guillaume-las@hotmail.fr>
2024-07-25 00:15:43 +0200

    finished layout need fixes to responsive

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,44 @@ $(window).scroll(function () {
     $("#nav").removeClass("scrolled");
   }
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const counters = document.querySelectorAll(".counter");
+
+  const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.5,
+  };
+
+  const easeOutQuad = (t) => t * (2 - t);
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const counter = entry.target;
+        const endValue = parseInt(counter.getAttribute("data-end"));
+        const duration = 2000;
+        const startTime = performance.now();
+
+        const animate = (currentTime) => {
+          const elapsedTime = currentTime - startTime;
+          const progress = Math.min(elapsedTime / duration, 1);
+          const easedProgress = easeOutQuad(progress);
+          counter.innerText = Math.floor(easedProgress * endValue);
+
+          if (progress < 1) {
+            requestAnimationFrame(animate);
+          }
+        };
+
+        requestAnimationFrame(animate);
+        observer.unobserve(counter);
+      }
+    });
+  }, options);
+
+  counters.forEach((counter) => {
+    observer.observe(counter);
+  });
+});
